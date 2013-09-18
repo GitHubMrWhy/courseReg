8d17f60cfd1496abc85eafacdd93d079d0af0951
lobsterfromaine <peterxumsh@gmail.com>
2013-09-18 03:39:08 -0400

    create storyboard

---
+++ courseReg/LoginScreenViewController.m
@@ -0,0 +1,47 @@
+//
+//  LoginScreenViewController.m
+//  courseReg
+//
+//  Created by Mingsheng Xu on 9/18/13.
+//  Copyright (c) 2013 Mingsheng Xu. All rights reserved.
+//
+
+#import "LoginScreenViewController.h"
+#import "API.h"
+
+@interface LoginScreenViewController ()
+
+@end
+
+@implementation LoginScreenViewController
+@synthesize username_TextField;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //focus on the username field / show keyboard
+    [username_TextField becomeFirstResponder];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)login_Click:(UIButton *)sender {
+}
+
+- (IBAction)reg_Click:(UIButton *)sender {
+}
+@end
